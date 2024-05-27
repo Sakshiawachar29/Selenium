805c4f129b7688708162143a616dcae4bcd57301
Sakshi  Awachar <163093719+Sakshiawachar29@users.noreply.github.com>
2024-05-27 11:11:09 +0530

    commit

---
+++ main/myprogram1/src/selenium/OpenEdgeBrowser.java
@@ -0,0 +1,22 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class OpenEdgeBrowser {
+
+	public static void main(String[] args) 
+	{
+		 System.out.println("hi");
+
+		 System.setProperty("webdriver.edge.driver", "driver\\msedgedriver.exe");
+	       
+
+		    WebDriver driver = new EdgeDriver();
+
+		        driver.manage().window().maximize();
+		        driver.get("http//www.google.com");
+		    }
+
+	}
+
